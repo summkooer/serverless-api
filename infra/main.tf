a46c1f8d174d8f07f779e5d33e2ac80376c5b00c
summkooer <xiarui1103@gmail.com>
2022-07-10 14:55:23 -0500

    switch accounts

--- infra/main.tf
+++ infra/main.tf
@@ -1,7 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
+}
 provider "aws" {
-  region = var.region
-  profile = var.aws_profile
-  shared_credentials_file =  var.shared_credentials_file
+  shared_config_files = [var.shared_config_files]
+  shared_credentials_files =  [var.shared_credentials_files]
   default_tags{
     tags = var.tags
   }
