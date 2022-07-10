e2900d5522b96ed784f0eeda53fc8b5c43f78be1
summkooer <xiarui1103@gmail.com>
2022-07-09 19:47:28 -0500

    add tags

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = var.region
   profile = var.aws_profile
   shared_credentials_file =  var.shared_credentials_file
+  default_tags{
+    tags = var.tags
+  }
 }
 
 variable "app_name" {
--- infra/variables.tf
+++ infra/variables.tf
@@ -14,3 +14,12 @@ variable "shared_credentials_file"{
     type = string
     default = "C:/Users/xiaru/.aws/credentials"
 }
+
+variable "tags"{
+    description = "A map of tags to add to all resources."
+    type = map(string)
+    default = {
+        application = "serverless-api"
+        env = "dev"
+    }
+}
